6e09513f8d8b86cd354c8b44db9cf81985e1ee0f
janky.hu <janky.hu@gmail.com>
2016-01-25 16:41:57 +0800

    create empty project

---
+++ memory-cache-obj-c/memory-cache-obj-c/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  memory-cache-obj-c
+//
+//  Created by Janky on 1/25/16.
+//  Copyright © 2016 janky.xm. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ memory-cache-obj-c/memory-cache-obj-c/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  memory-cache-obj-c
+//
+//  Created by Janky on 1/25/16.
+//  Copyright © 2016 janky.xm. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
